905e18ff838bbfc9326f805c72197f3882aef005
Shane Carr <shane.carr@wustl.edu>
2016-03-28 23:50:23 -0500

    Adding metadata files for PyPI

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+    name = "basc",
+    packages = ["basc"], # this must be the same as the name above
+    version = "1.0.0",
+    description = "A framework for rapid searches for adsorbate-surface structures",
+    author = "Shane F. Carr",
+    author_email = "shane.carr@wustl.edu",
+    url = "https://gitlab.com/caml/basc",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3"
+    ]
+)
